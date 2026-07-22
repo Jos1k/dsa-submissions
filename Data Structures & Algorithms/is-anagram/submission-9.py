fd45f3b520e959ba6995a7441384b13fcfb228e6
Ihor Hadzera <ihor.hadzera@gmail.com>
2026-07-22 13:44:18 +0200

    Add: is-anagram - submission-9

---
+++ Data Structures & Algorithms/is-anagram/submission-9.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        s_d, t_d = {}, {}
+
+        if len(s) != len(t):
+            return False
+
+        for i in range(len(s)):
+            s_i = s_d.setdefault(s[i], 0)
+            s_d[s[i]] = s_i + 1
+            t_i = t_d.setdefault(t[i], 0)
+            t_d[t[i]] = t_i + 1
+
+        for k,v in s_d.items():
+            
+            if v != t_d.get(k, 0):
+                return False
+        
+        return True
